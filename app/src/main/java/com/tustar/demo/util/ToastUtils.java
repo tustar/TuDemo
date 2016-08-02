832daf9046258f9368cd881c0005cd51fe97b9d4
tuxp1 <tuxp1@zuk.com>
2016-08-02 15:50:44 +0800

    Add ToastUtils

---
+++ app/src/main/java/com/tustar/demo/util/ToastUtils.java
@@ -0,0 +1,34 @@
+import android.widget.Toast;
+
+gstpackage com.tustar.demo.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by tustar on 16-8-2.
+ */
+public class ToastUtils {
+
+    private static Toast mToast;
+
+    public static void show(Context context, int resId, int duration) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, resId, duration);
+        } else {
+            mToast.setText(resId);
+        }
+
+        mToast.show();
+    }
+
+    public static void show(Context context, CharSequence text, int duration) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, text, duration);
+        } else {
+            mToast.setText(text);
+        }
+
+        mToast.show();
+    }
+}
