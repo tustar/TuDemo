707095ffcab8c324b7ae3676a938a102342523d1
tustar <tustar1030@126.com>
2016-11-13 21:24:03 +0800

    Finish history layout

--- app/src/main/java/com/tustar/demo/widget/dragview/HistoryLayout.java
+++ app/src/main/java/com/tustar/demo/widget/dragview/HistoryLayout.java
@@ -3,17 +3,12 @@ package com.tustar.demo.widget.dragview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewConfigurationCompat;
 import android.support.v4.widget.ViewDragHelper;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
-import android.widget.OverScroller;
 import android.widget.TextView;
 
 import com.tustar.demo.R;
@@ -35,24 +30,51 @@ public class HistoryLayout extends FrameLayout {
     private int mContentViewId = 0;
     private View mTopView;
     private View mContentView;
+    private int mTopViewHeight;
+    private int mContentViewHeight;
+    private int mContentViewTop = 0;
 
-    // Position
-    private int mLastX;
-    private int mLastY;
-    private int mDownX;
-    private int mDownY;
-
+    private boolean mScrollable = true;
     private ViewDragHelper mViewDragHelper;
-    private int mTouchSlop;
-    private OverScroller mScroller;
-    private VelocityTracker mVelocityTracker;
-    private int mScaledMinimumFlingVelocity;
-    private int mScaledMaximumFlingVelocity;
+    private ViewDragHelper.Callback mCallback = new ViewDragHelper.Callback() {
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return child == mContentView;
+        }
 
-    //
-    private boolean mScrollable;
-    private boolean mDraggingInVertical;
-    private int mScrollerDuration = DEFAULT_SCROLLER_DURATION;
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return 0;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            Logger.i(TAG, "clampViewPositionVertical :: top = " + top
+                    + ", dy = " + dy);
+            if (!mScrollable) {
+                return 0;
+            }
+            return top;
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            Logger.i(TAG, "onViewReleased :: xvel = " +
+                    xvel + ", yvel = " + yvel);
+            super.onViewReleased(releasedChild, xvel, yvel);
+            int top = mContentView.getTop();
+            Logger.d(TAG, "onViewReleased :: top = " + top);
+//            if (top < mContentViewHeight / 2) {
+//                mViewDragHelper.smoothSlideViewTo(mTopView, 0, mContentViewTop - mTopViewHeight);
+//                mViewDragHelper.smoothSlideViewTo(mContentView, 0, mContentViewTop);
+//                ViewCompat.postInvalidateOnAnimation(HistoryLayout.this);
+//            } else {
+//                mViewDragHelper.smoothSlideViewTo(mTopView, 0, 0);
+//                mViewDragHelper.smoothSlideViewTo(mContentView, 0, mHeight);
+//                ViewCompat.postInvalidateOnAnimation(HistoryLayout.this);
+//            }
+        }
+    };
 
     public HistoryLayout(Context context) {
         this(context, null);
@@ -71,9 +93,20 @@ public class HistoryLayout extends FrameLayout {
         init(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.HistoryLayout,
+                defStyleAttr, defStyleRes);
+        mTopViewId = ta.getResourceId(R.styleable.HistoryLayout_top, mTopViewId);
+        mContentViewId = ta.getResourceId(R.styleable.HistoryLayout_content, mContentViewId);
+        ta.recycle();
+
+        mViewDragHelper = ViewDragHelper.create(this, mCallback);
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Logger.d("onMeasure :: ");
+        Logger.d(TAG, "onMeasure :: ");
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
@@ -82,7 +115,7 @@ public class HistoryLayout extends FrameLayout {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mWidth = getMeasuredWidth();
         mHeight = getMeasuredHeight();
-        Logger.d("onMeasure :: mWidth = " + mWidth + ", mHeight = " + mHeight);
+        Logger.d(TAG, "onMeasure :: mWidth = " + mWidth + ", mHeight = " + mHeight);
     }
 
     @Override
@@ -106,114 +139,54 @@ public class HistoryLayout extends FrameLayout {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Logger.i(TAG, "onLayout :: ");
+        int parentHeight = getMeasuredHeightAndState();
         if (mContentView != null) {
             int contentViewWidth = mContentView.getMeasuredWidthAndState();
             int contentViewHeight = mContentView.getMeasuredHeightAndState();
-            LayoutParams params = (LayoutParams) mContentView.getLayoutParams();
+            LayoutParams lp = (LayoutParams) mContentView.getLayoutParams();
             int l = mContentView.getPaddingLeft();
-            int t = mContentView.
-
+            int t = parentHeight - contentViewHeight + lp.topMargin;
+            mContentViewTop = t;
+            int r = l + contentViewWidth;
+            int b = t + contentViewHeight;
+            mContentView.layout(l, t, r, b);
         }
-        super.onLayout(changed, left, top, right, bottom);
+        if (mTopView != null) {
+            int topViewWidth = mTopView.getMeasuredWidthAndState();
+            int topViewHeight = mTopView.getMeasuredHeightAndState();
+            LayoutParams lp = (LayoutParams) mTopView.getLayoutParams();
+            int l = mTopView.getPaddingLeft();
+            int t = mContentViewTop + lp.topMargin - topViewHeight;
+            int r = l + topViewWidth;
+            int b = t + topViewHeight;
+            mTopView.layout(l, t, r, b);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mTopViewHeight = mTopView.getHeight();
+        mContentViewHeight = mContentView.getHeight();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return mViewDragHelper.shouldInterceptTouchEvent(event);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (mVelocityTracker == null) {
-            mVelocityTracker = VelocityTracker.obtain();
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mViewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
         }
-        mVelocityTracker.addMovement(event);
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                mDownX = mLastX = (int) event.getX();
-                mDownY = mLastY = (int) event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                // 不可滑动
-                if (!mScrollable) {
-                    break;
-                }
-                int dx = (int) (mLastX - event.getX());
-                int dy = (int) (mLastY - event.getY());
-                // 判断是否是上下滑动
-                if (!mDraggingInVertical && Math.abs(dy) > mTouchSlop && Math.abs(dy) > Math.abs(dx)) {
-                    mDraggingInVertical = true;
-                }
-                //
-                if (mDraggingInVertical) {
-                    scrollBy(0, dy);
-                    mLastX = (int) getX();
-                    mLastY = (int) getY();
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                mDraggingInVertical = false;
-                mVelocityTracker.computeCurrentVelocity(1000, mScaledMaximumFlingVelocity);
-                int velocityY = (int) mVelocityTracker.getYVelocity();
-                int velocity = Math.abs(velocityY);
-                if (velocity > mScaledMinimumFlingVelocity) {
-                    int duration = getYDuration(event, velocity);
-                    if (velocityY < 0) {
-                        smoothOpenTop(duration);
-                    } else {
-                        smoothCloseTop(duration);
-                    }
-                    ViewCompat.postInvalidateOnAnimation(this);
-                }
-                mVelocityTracker.clear();
-                mVelocityTracker.recycle();
-                mVelocityTracker = null;
-                //
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                mDraggingInVertical = false;
-                if (!mScroller.isFinished()) {
-                    mScroller.abortAnimation();
-                }
-                break;
-        }
-        return super.onTouchEvent(event);
-    }
-
-    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.HistoryLayout,
-                defStyleAttr, defStyleRes);
-        mTopViewId = ta.getResourceId(R.styleable.HistoryLayout_top, mTopViewId);
-        mContentViewId = ta.getResourceId(R.styleable.HistoryLayout_content, mContentViewId);
-        ta.recycle();
-
-        ViewConfiguration config = ViewConfiguration.get(context);
-        mTouchSlop = ViewConfigurationCompat.getScaledPagingTouchSlop(config);
-        mScroller = new OverScroller(context);
-        mScaledMinimumFlingVelocity = config.getScaledMinimumFlingVelocity();
-        mScaledMaximumFlingVelocity = config.getScaledMaximumFlingVelocity();
-    }
-
-    private void smoothOpenTop(int duration) {
-    }
-
-    private void smoothCloseTop(int duration) {
-    }
-
-
-    private int getYDuration(MotionEvent event, int velocity) {
-        int sy = getScrollY();
-        int dy = (int) (event.getY() - sy);
-        final int height = mTopView.getHeight();
-        final int halfHeight = height / 2;
-        final float ratio = Math.min(1.0f, 1.0f * Math.abs(dy) / height);
-        final float distance = halfHeight + halfHeight * ratio;
-        int duration;
-        if (velocity > 0) {
-            duration = 4 * Math.round(1000 * Math.abs(distance / velocity));
-        } else {
-            final float pageDetal = (float) Math.abs(dy) / height;
-            duration = (int) ((pageDetal + 1) * 100);
-        }
-        duration = Math.min(duration, mScrollerDuration);
-        return duration;
     }
 
     // Setter & Getter
@@ -223,21 +196,5 @@ public class HistoryLayout extends FrameLayout {
 
     public void setScrollable(boolean scrollable) {
         this.mScrollable = scrollable;
-    }
-
-    public boolean isDraggingInVertical() {
-        return mDraggingInVertical;
-    }
-
-    public void setDraggingInVertical(boolean draggingInVertical) {
-        this.mDraggingInVertical = draggingInVertical;
-    }
-
-    public int getScrollerDuration() {
-        return mScrollerDuration;
-    }
-
-    public void setScrollerDuration(int scrollerDuration) {
-        this.mScrollerDuration = scrollerDuration;
     }
 }
