ee50eae20d88ac364aa06aae9ee95f5f297eccad
tuxp1 <tuxp1@lenovo.com>
2017-08-16 18:25:20 +0800

    Finish android drawable

--- app/src/main/java/com/tustar/demo/module/ryg/ch6/RygCh6Activity.kt
+++ app/src/main/java/com/tustar/demo/module/ryg/ch6/RygCh6Activity.kt
@@ -2,12 +2,16 @@ package com.tustar.demo.module.ryg.ch6
 
 import android.os.Bundle
 import com.tustar.demo.R
-import com.tustar.demo.base.BaseActivity
 import com.tustar.demo.module.ryg.ch5.BaseRygActivity
 
 class RygCh6Activity : BaseRygActivity() {
 
+    init {
+        sClassList.add(RygCh6DrawableActivity::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        ryg_data_source = R.array.ryg_ch6_list
         super.onCreate(savedInstanceState)
         title = getString(R.string.ryg_ch6_title)
     }
