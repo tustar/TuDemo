68d61877faea89b0c5c01494126f37983c260285
tuxp1 <tuxp1@lenovo.com>
2017-08-21 16:54:05 +0800

    Finish chapter07 demos

---
+++ app/src/main/java/com/tustar/demo/module/ryg/ch7/RygCh7ActivityAnimatorActivity.kt
@@ -0,0 +1,19 @@
+package com.tustar.demo.module.ryg.ch7
+
+import android.os.Bundle
+import com.tustar.demo.R
+import com.tustar.demo.base.BaseActivity
+
+class RygCh7ActivityAnimatorActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ryg_ch7_activity_animator)
+        title = getString(R.string.ryg_ch7_activity_animator)
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.ryg_ch7_enter_anim, R.anim.ryg_ch7_exit_anim)
+    }
+}
