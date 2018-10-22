a24635dc4a0caa1ee1e4f378b53b61442b852a50
tuxp1 <tuxp1@lenovo.com>
2018-10-22 19:49:36 +0800

    [TuDemo][InDb]Finish InDb function

--- jetpack/src/main/java/com/tustar/jetpack/reddit/repository/inDb/DbRedditPostRepository.kt
+++ jetpack/src/main/java/com/tustar/jetpack/reddit/repository/inDb/DbRedditPostRepository.kt
@@ -1,10 +1,19 @@
 package com.tustar.jetpack.reddit.repository.inDb
 
+import androidx.annotation.MainThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
 import com.tustar.jetpack.reddit.api.RedditApi
 import com.tustar.jetpack.reddit.db.RedditDb
 import com.tustar.jetpack.reddit.repository.Listing
+import com.tustar.jetpack.reddit.repository.NetworkState
 import com.tustar.jetpack.reddit.repository.RedditPostRepository
 import com.tustar.jetpack.reddit.vo.RedditPost
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.concurrent.Executor
 
 class DbRedditPostRepository(
@@ -16,7 +25,73 @@ class DbRedditPostRepository(
         private const val DEFAULT_NETWORK_PAGE_SIZE = 10
     }
 
+    @MainThread
+    private fun insertResultIntoDb(subredditName: String, body: RedditApi.ListingResponse?) {
+        body!!.data.children.let { posts ->
+            db.runInTransaction {
+                val start = db.posts().getNextIndexInSubreddit(subredditName)
+                val items = posts.mapIndexed { index, child ->
+                    child.data.indexInResponse = start + index
+                    child.data
+                }
+                db.posts().insert(items)
+            }
+        }
+    }
+
+    @MainThread
+    private fun refresh(subredditName: String): LiveData<NetworkState> {
+        val networkState = MutableLiveData<NetworkState>()
+        networkState.value = NetworkState.LOADING
+        redditApi.getTop(subredditName, networkPageSize).enqueue(
+                object : Callback<RedditApi.ListingResponse> {
+                    override fun onFailure(call: Call<RedditApi.ListingResponse>, t: Throwable) {
+                        networkState.value = NetworkState.error(t.message)
+                    }
+
+                    override fun onResponse(call: Call<RedditApi.ListingResponse>,
+                                            response: Response<RedditApi.ListingResponse>) {
+
+                        ioExecutor.execute {
+                            db.runInTransaction {
+                                db.posts().deleteBySubreddit(subredditName)
+                                insertResultIntoDb(subredditName, response.body())
+                            }
+                            //
+                            networkState.postValue(NetworkState.LOADED)
+                        }
+                    }
+                }
+        )
+        return networkState
+    }
+
+    @MainThread
     override fun postsOfSubreddit(subReddit: String, pageSize: Int): Listing<RedditPost> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val boundaryCallback = SubredditBoundaryCallback(
+                webservice = redditApi,
+                subredditName = subReddit,
+                handleResponse = this::insertResultIntoDb,
+                ioExecutor = ioExecutor,
+                networkPageSize = networkPageSize)
+        val dataSourceFactory = db.posts().postsBySubreddit(subReddit)
+        val builder = LivePagedListBuilder(dataSourceFactory, pageSize)
+                .setBoundaryCallback(boundaryCallback)
+
+        val refreshTrigger = MutableLiveData<Unit>()
+        val refreshState = Transformations.switchMap(refreshTrigger, {
+            refresh(subReddit)
+        })
+
+        return Listing(pagedList = builder.build(),
+                networkState = boundaryCallback.networkState,
+                retry = {
+                    boundaryCallback.helper.retryAllFailed()
+                },
+                refresh = {
+                    refreshTrigger.value = null
+                },
+                refreshState = refreshState
+        )
     }
 }
--- jetpack/src/main/java/com/tustar/jetpack/reddit/repository/inDb/SubredditBoundaryCallback.kt
+++ jetpack/src/main/java/com/tustar/jetpack/reddit/repository/inDb/SubredditBoundaryCallback.kt
@@ -1,8 +1,14 @@
 package com.tustar.jetpack.reddit.repository.inDb
 
+import androidx.annotation.MainThread
 import androidx.paging.PagedList
+import com.androidx.paging.PagingRequestHelper
 import com.tustar.jetpack.reddit.api.RedditApi
+import com.tustar.jetpack.reddit.util.createStatusLiveData
 import com.tustar.jetpack.reddit.vo.RedditPost
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.concurrent.Executor
 
 class SubredditBoundaryCallback(
@@ -13,4 +19,54 @@ class SubredditBoundaryCallback(
         private val networkPageSize: Int)
     : PagedList.BoundaryCallback<RedditPost>() {
 
+    val helper = PagingRequestHelper(ioExecutor)
+    val networkState = helper.createStatusLiveData()
+
+    @MainThread
+    override fun onZeroItemsLoaded() {
+        helper.runIfNotRunning(PagingRequestHelper.RequestType.INITIAL) {
+            webservice.getTop(
+                    subreddit = subredditName,
+                    limit = networkPageSize).enqueue(
+                    createWebserviceCallback(it))
+        }
     }
+
+    @MainThread
+    override fun onItemAtEndLoaded(itemAtEnd: RedditPost) {
+        helper.runIfNotRunning(PagingRequestHelper.RequestType.AFTER) {
+            webservice.getTopAfter(
+                    subreddit = subredditName,
+                    after = itemAtEnd.name,
+                    limit = networkPageSize)
+                    .enqueue(createWebserviceCallback(it))
+        }
+    }
+
+    override fun onItemAtFrontLoaded(itemAtFront: RedditPost) {
+        // ignored
+    }
+
+    private fun insertItemsIntoDb(response: Response<RedditApi.ListingResponse>,
+                                  it: PagingRequestHelper.Request.Callback) {
+        ioExecutor.execute {
+            handleResponse(subredditName, response.body())
+            it.recordSuccess()
+        }
+    }
+
+
+    private fun createWebserviceCallback(it: PagingRequestHelper.Request.Callback)
+            : Callback<RedditApi.ListingResponse> {
+        return object : Callback<RedditApi.ListingResponse> {
+            override fun onFailure(call: Call<RedditApi.ListingResponse>, t: Throwable) {
+                it.recordFailure(t)
+            }
+
+            override fun onResponse(call: Call<RedditApi.ListingResponse>,
+                                    response: Response<RedditApi.ListingResponse>) {
+                insertItemsIntoDb(response, it)
+            }
+        }
+    }
+}
